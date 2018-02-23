b31b7c5ac477b5f57d35020039d1c9601f37cb3d
Ola Tarkowska <olat@ebi.ac.uk>
2018-02-23 09:37:17 +0000

    add license

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright 2017 EMBL - European Bioinformatics Institute
+# Copyright 2018 EMBL - European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
