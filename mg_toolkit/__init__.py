32a626331fe71f71df27938d77fb0038b9bd37ee
Ola Tarkowska <olat@ebi.ac.uk>
2018-07-04 07:49:26 +0100

    bump version

--- mg_toolkit/__init__.py
+++ mg_toolkit/__init__.py
@@ -23,7 +23,7 @@ import mg_toolkit
 from .metadata import original_metadata  # noqa
 from .search import sequence_search  # noqa
 
-__version__ = '0.2.7'
+__version__ = '0.3.0'
 __all__ = [
     'original_metadata',
     'sequence_search',
