455b223ad3f76e7c8b664cf2fc99b2ea2d12fa29
Martin Beracochea <mbc@ebi.ac.uk>
2020-11-03 16:06:06 +0000

    Bump version: 0.7.0 → 0.8.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["ez_setup"]),
-    version="0.7.0",
+    version="0.8.0",
     python_requires=">=3.5",
     install_requires=install_requirements,
     setup_requires=["pytest-runner"],
