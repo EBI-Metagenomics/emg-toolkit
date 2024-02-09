733fd2a4ab006c02e052397a54d274383eeb262d
Martin Beracochea <mbc@ebi.ac.uk>
2024-02-09 16:08:30 +0000

    Testing

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -28,9 +28,6 @@ rm -rf MGYS00002478/
 mg-toolkit -d bulk_download -a MGYS00002478 -p 4.1 -g non-coding_rnas
 set +x
 
-cat MGYS00002478/MGYS00002478_metadata.tsv
-md5sum MGYS00002478/MGYS00002478_metadata.tsv
-
 md5sum -c bulk_download/ERR169332.5_8S_rRNA.RF00002.fa.md5
 md5sum -c bulk_download/MGYS00002478_metadata.tsv.md5
 rm -r MGYS00002478
